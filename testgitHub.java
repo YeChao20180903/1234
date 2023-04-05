11233bdce59650a6495573750ea1837a42ebfc2b
YeChao20180903 <42921616+YeChao20180903@users.noreply.github.com>
2023-04-05 18:45:25 +0800

    Update testgitHub.java

--- testgitHub.java
+++ testgitHub.java
@@ -2,5 +2,6 @@ public class testgitHub {
     public static void main(String[] args) {
         System.out.println("Hello hogwarts");
         System.out.println("pull test");
+        System.out.println("add2");
     }
 }
